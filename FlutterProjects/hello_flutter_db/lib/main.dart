2b8c507bf87b28046f1902bc9dac8b46469019af
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2023-10-05 19:37:36 +0900

    updates

--- FlutterProjects/hello_flutter_db/lib/main.dart
+++ FlutterProjects/hello_flutter_db/lib/main.dart
@@ -1,15 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 //
 
-class NamesDto {
+class Name {
   final String name;
   final String nameHira;
   final String nameEn;
 
-  NamesDto({required this.name, required this.nameHira, required this.nameEn});
+  Name({
+    required this.name,
+    required this.nameHira,
+    required this.nameEn,
+  });
 }
 
 class NamesDao {
@@ -17,12 +25,12 @@ class NamesDao {
   NamesDao._internal();
   factory NamesDao() => instance;
 
-  Future<Map<String, NamesDto>> getAll(Database database) async {
+  Future<Map<String /*name*/, Name>> getAll(Database database) async {
     List<Map> list = await database.rawQuery('SELECT * FROM names');
-    var buffer = <String, NamesDto>{};
+    var buffer = <String, Name>{};
     for (final row in list) {
       final name = row['name'].toString();
-      buffer[name] = NamesDto(
+      buffer[name] = Name(
         name: name,
         nameHira: row['name_hira'].toString(),
         nameEn: row['name_en'].toString(),
@@ -32,13 +40,77 @@ class NamesDao {
   }
 }
 
+class Poi {
+  final String name;
+  final double latitude;
+  final double longitude;
+  final String prefecture;
+
+  Poi({
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+    required this.prefecture,
+  });
+}
+
+class PoisDao {
+  static final PoisDao instance = PoisDao._internal();
+  PoisDao._internal();
+  factory PoisDao() => instance;
+
+  Future<Map<String /*name*/, Poi>> getAll(Database database) async {
+    List<Map> list = await database.rawQuery('SELECT * FROM pois');
+    var buffer = <String, Poi>{};
+    for (final row in list) {
+      final name = row['name'].toString();
+      buffer[name] = Poi(
+        name: name,
+        latitude: double.parse(row['latitude'].toString()),
+        longitude: double.parse(row['longitude'].toString()),
+        prefecture: row['prefecture'].toString(),
+      );
+    }
+    return buffer;
+  }
+}
+
 //
 
-final dbProvider = FutureProvider<Database>(
-  (ref) async => await openDatabase('data/pois.sqlite3'),
-);
+final dbProvider = FutureProvider<Database>((ref) async {
+  final path = join(await getDatabasesPath(), 'pois.sqlite3');
+  final exists = await databaseExists(path);
+  if (!exists) {
+    print('!exists');
+    await Directory(dirname(path)).create(recursive: true);
+    print('data');
+    final data = await rootBundle.load(url.join('assets', 'pois.sqlite3'));
+    print('bytes');
+    final bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    print('write');
+    await File(path).writeAsBytes(bytes, flush: true);
+  }
+  print('openDb');
+  return await openDatabase(path, readOnly: true);
+});
+
+final namesProvider = FutureProvider<Map<String, Name>>((ref) async {
+  final db = await ref.watch(dbProvider.future);
+  return NamesDao.instance.getAll(db);
+});
+
+final poisProvider = FutureProvider<Map<String, Poi>>((ref) async {
+  final db = await ref.watch(dbProvider.future);
+  return PoisDao.instance.getAll(db);
+});
 
 void main() {
+  if (Platform.isWindows || Platform.isLinux) {
+    // Initialize FFI
+    sqfliteFfiInit();
+  }
+  databaseFactory = databaseFactoryFfi;
+
   runApp(
     const ProviderScope(
       child: MyApp(),
@@ -52,6 +124,8 @@ class MyApp extends ConsumerWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final pois = ref.watch(poisProvider);
+
     return MaterialApp(
       title: 'POIs',
       theme: ThemeData(
@@ -59,11 +133,11 @@ class MyApp extends ConsumerWidget {
         useMaterial3: false,
       ),
       home: Scaffold(
-        appBar: AppBar(title: const Text('Example')),
-        body: const Center(
-          child: Row(
-            children: [],
-          ),
+        appBar: AppBar(title: const Text('POIs')),
+        body: pois.when(
+          data: (data) => Center(child: Text(data.keys.join(','))),
+          error: (error, _) => Center(child: Text(error.toString())),
+          loading: () => const Center(child: CircularProgressIndicator()),
         ),
       ),
     );
