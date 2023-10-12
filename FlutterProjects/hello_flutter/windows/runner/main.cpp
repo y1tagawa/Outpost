79b323f99283b2ab90d054476c20b08a76eb6957
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2023-10-13 06:38:36 +0900

    refactor

--- FlutterProjects/hello_flutter/windows/runner/main.cpp
+++ FlutterProjects/hello_flutter/windows/runner/main.cpp
@@ -25,8 +25,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(800, 10);
-  Win32Window::Size size(320, 640);
+  Win32Window::Point origin(810, 10);
+  Win32Window::Size size(300, 600);
   if (!window.CreateAndShow(L"hello_flutter", origin, size)) {
     return EXIT_FAILURE;
   }
