f606e94f16052d59703737db360a184a99ac79e6
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2023-10-17 10:08:41 +0900

    update

--- FlutterProjects/hello_flutter_cube_simple/lib/main.dart
+++ FlutterProjects/hello_flutter_cube_simple/lib/main.dart
@@ -4,6 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cube/flutter_cube.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+final sceneProvider = StateProvider<Scene?>((ref) => null);
+final textureOnProvider = StateProvider<bool>((ref) => true);
+
+Object? _cube;
+Object? _cube2;
+
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -13,10 +19,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // void cprint(String name, int c) {
+    //   print(name);
+    //   print(((c & 0xFF0000) >> 16) / 256.0);
+    //   print(((c & 0x00FF00) >> 8) / 256.0);
+    //   print((c & 0x0000FF) / 256.0);
+    // }
+    //
+    // cprint('Blue', 0x334ccc);
+    // cprint('Purple', 0xcc3399);
+    // cprint('Orange', 0xd99d26);
+    // cprint('Green', 0x33cc66);
+    // cprint('LtGrey', 0x999999);
+    // cprint('DkGrey', 0x333333);
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: const MyHomePage(title: 'Hello Flutter Cube'),
+      home: const MyHomePage(title: 'Hello Flutter \'Cube\''),
     );
   }
 }
@@ -28,21 +48,48 @@ class MyHomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final scene = ref.watch(sceneProvider);
+    final textureOn = ref.watch(textureOnProvider);
+
     void onSceneCreated(Scene scene) {
       scene.camera.position.setValues(3, 2, 3);
       scene.camera.fov = 45;
 
-      final cube = Object(
+      _cube = Object(
         position: Vector3.zero(),
         backfaceCulling: true,
         fileName: 'assets/cube/cube.obj',
       );
-      scene.world.add(cube);
+      _cube2 = Object(
+        position: Vector3.zero(),
+        backfaceCulling: true,
+        fileName: 'assets/cube/cube2.obj',
+      );
+      scene.world.add(textureOn ? _cube! : _cube2!);
+      ref.read(sceneProvider.notifier).state = scene;
+    }
+
+    if (scene != null) {
+      final (show, hide) = textureOn ? (_cube, _cube2) : (_cube2, _cube);
+      if (scene.world.children.contains(hide!)) {
+        scene.world.remove(hide);
+      }
+      if (!scene.world.children.contains(show!)) {
+        scene.world.add(show);
+      }
     }
 
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.read(textureOnProvider.notifier).state = !textureOn;
+            },
+            icon: const Icon(Icons.onetwothree),
+          ),
+        ],
       ),
       body: Cube(
         onSceneCreated: onSceneCreated,
