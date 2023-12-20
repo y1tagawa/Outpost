140626401227307222c719e6465cabcb25b5c927
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2023-12-20 21:28:17 +0900

    Update main.dart

--- FlutterProjects/hello_flutter_isolate/lib/main.dart
+++ FlutterProjects/hello_flutter_isolate/lib/main.dart
@@ -1,7 +1,39 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:isolate';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+
+final logger = Logger('hello_flutter_isolate');
+
+late final ReceivePort clientReceivePort;
+late final StreamIterator<Object?> clientReceiveIterator;
+
+void main() async {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    debugPrint('[${record.loggerName}] ${record.level.name}: ${record.time}: ${record.message}');
+  });
+
+  clientReceivePort = ReceivePort("clientReceivePort");
+  clientReceiveIterator = StreamIterator(clientReceivePort);
+
+  await Isolate.spawn(_server, clientReceivePort.sendPort);
+
   runApp(const MyApp());
+}
+
+void _server(SendPort sendPort) async {
+  final serverLogger = Logger('serverLogger');
+
+  serverLogger.fine('[i] _server');
+  //await for (;;) {
+  serverLogger.fine('_server 1');
+  sendPort.send(1);
+  serverLogger.fine('_server 2');
+  sendPort.send(2);
+  //}
+  serverLogger.fine('[o] _server');
 }
 
 class MyApp extends StatelessWidget {
@@ -30,13 +62,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int _digit = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -50,19 +76,27 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Digit is:',
             ),
             Text(
-              '$_counter',
+              '$_digit',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () async {
+          await clientReceiveIterator.moveNext();
+          setState(() {
+            if (clientReceiveIterator.current is int) {
+              _digit = clientReceiveIterator.current as int;
+              logger.fine('client received from port: $_digit');
+            }
+          });
+        },
+        tooltip: 'Next',
+        child: const Icon(Icons.navigate_next),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
