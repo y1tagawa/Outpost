e74cfce5131ae97364efacf8680c638160a6662d
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2023-12-20 22:24:27 +0900

    Update main.dart

--- FlutterProjects/hello_flutter_isolate/lib/main.dart
+++ FlutterProjects/hello_flutter_isolate/lib/main.dart
@@ -10,23 +10,30 @@ late final ReceivePort receiveFromServerPort;
 late final StreamIterator<Object?> receiveFromServerIterator;
 late final SendPort sendToServerPort;
 
+/*
+  クライアント
+ */
 void main() async {
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((record) {
     debugPrint('[${record.loggerName}] ${record.level.name}: ${record.time}: ${record.message}');
   });
 
+  //  サーバ起動
   receiveFromServerPort = ReceivePort("clientReceivePort");
   receiveFromServerIterator = StreamIterator(receiveFromServerPort);
-
   await Isolate.spawn(_server, receiveFromServerPort.sendPort);
 
+  //  最初にサーバへのSendPortを受け取る
   await receiveFromServerIterator.moveNext();
   sendToServerPort = receiveFromServerIterator.current as SendPort;
 
   runApp(const MyApp());
 }
 
+/*
+  クライアントとキャッチボールするサーバ
+ */
 void _server(SendPort sendPort) async {
   final isolateLogger = Logger('isolateLogger');
 
@@ -39,13 +46,18 @@ void _server(SendPort sendPort) async {
   isolateLogger.fine('[i] _server');
 
   final receiveFromClientPort = ReceivePort('receiveFromClientPort');
-  sendPort.send(receiveFromClientPort.sendPort);
   final receiveFromClientIterator = StreamIterator(receiveFromClientPort);
 
+  // 最初にサーバへのSendPortを送る
+  sendPort.send(receiveFromClientPort.sendPort);
+
+  // 以後、クライアントからの入力を待ち、返信する無限ループに入る。
   for (;;) {
     isolateLogger.fine('_server awaits');
+    // クライアントからの入力待ち
     await receiveFromClientIterator.moveNext();
-    final input = receiveFromClientIterator.current;
+    final input = receiveFromClientIterator.current as int;
+    // 返信
     isolateLogger.fine('_server received input=[$input] and sending ${input + 1}');
     sendPort.send(input + 1);
   }
@@ -78,7 +90,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _digit = 0;
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -92,10 +104,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Digit is:',
+              'Counter is:',
             ),
             Text(
-              '$_digit',
+              '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
@@ -103,16 +115,16 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          logger.fine('sending to server $_digit');
-          sendToServerPort.send(_digit);
-
+          // FABがクリックされたら、サーバに現在のカウンタ値を送る
+          logger.fine('sending to server $_counter');
+          sendToServerPort.send(_counter);
+          // サーバからの返信待ち
           logger.fine('client awaits');
           await receiveFromServerIterator.moveNext();
+          // 受け取った値でカウンタ更新
           setState(() {
-            if (receiveFromServerIterator.current is int) {
-              _digit = receiveFromServerIterator.current as int;
-              logger.fine('client received $_digit');
-            }
+            logger.fine('client received ${receiveFromServerIterator.current}');
+            _counter = receiveFromServerIterator.current as int;
           });
         },
         tooltip: 'Next',
