87f2eefccddd7a24297deff3529d2e738def0dab
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2023-12-28 22:19:44 +0900

    Update main.dart

--- FlutterProjects/hello_flutter_client_server/lib/main.dart
+++ FlutterProjects/hello_flutter_client_server/lib/main.dart
@@ -3,6 +3,7 @@
 /// todo: isolateによるモーダルなセッションのサンプル（JKP, AMHとか）
 
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -270,7 +271,8 @@ class _CounterClientState extends State<CounterClient> {
 enum Mode {
   jkp, // ジャンケンポン
   aks, // あいこでしょ
-  amh, // あっちむいてホイ
+  aiAmh, // AIがあっちむいてホイ
+  huAmh, // 人間があっちむいてホイ
   aiWin, // AIの勝ち
   huWin, // 人間の勝ち
   draw, // 引き分け
@@ -288,101 +290,135 @@ enum Gcp {
 enum Dir { up, down, left, right }
 
 abstract class LogicWrapper implements ISession {
-  final _iStreamController = StreamController<String>();
-  StreamIterator<(int, String)>? _oStreamIterator;
+  /// ロジックへの入力ストリーム（コントローラ）
+  final _is = StreamController<String>();
 
-  Stream<(int, String)> run(Stream<String> iStream);
+  /// ロジックからの出力ストリーム（イテレータ）
+  StreamIterator<(int, String)>? _os;
 
-  @override
-  Future<(int, String)> get(String unused) async {
-    if (_oStreamIterator == null) {
-      _oStreamIterator = StreamIterator(run(_iStreamController.stream));
-      await _oStreamIterator!.moveNext();
+  /// ロジック
+  /// 最初に一回getに対応して呼び出されるので、初回のyieldまでには必ず状態変数の初期化のみを行うこと。
+  Stream<(int, String)> run(
+    ///ロジックへの入力ストリーム
+    Stream<String> iStream,
+  );
+
+  // ロジックが起動されていない場合のみ初回起動する。
+  Future<void> _run() async {
+    if (_os == null) {
+      _os = StreamIterator(run(_is.stream));
+      await _os!.moveNext();
     }
-    return _oStreamIterator!.current;
   }
 
+  /// ロジックからの出力を、必要に応じ変換する。
+  /// 例えば状態変数が巨大なとき、ロジックの返値やメンバ変数等から一部抽出するなど。
+  @protected
+  String filterState(
+    /// getまたはpostの引数
+    String args,
+
+    /// ロジックの出力
+    String state,
+  ) {
+    // デフォルトではロジックの出力をそのまま返す。
+    return state;
+  }
+
+  /// ロジックが起動されていない場合は初回起動し、以後現在状態を返す。
+  @override
+  Future<(int, String)> get(String args) async {
+    await _run();
+    return (_os!.current.$1, filterState(args, _os!.current.$2));
+  }
+
+  /// ロジックが起動されていない場合は初回起動し、以後postの結果状態を返す。
   @override
   Future<(int, String)> post(String args) async {
-    if (_oStreamIterator == null) {
-      _oStreamIterator = StreamIterator(run(_iStreamController.stream));
-      await _oStreamIterator!.moveNext();
-    }
-    _iStreamController.add(args);
-    await _oStreamIterator!.moveNext();
-    return _oStreamIterator!.current;
+    await _run();
+    _is.add(args);
+    await _os!.moveNext();
+    return (_os!.current.$1, filterState(args, _os!.current.$2));
   }
 }
 
 /// ロジック
 class JkpAmh extends LogicWrapper {
+  // 状態変数
+  int _revision = 0;
+  Mode? _mode;
+  bool? _isJkp;
+  Gcp? _aiGcp;
+  Gcp? _huGcp;
+  Dir? _aiDir;
+  Dir? _huDir;
+
   @override
   Stream<(int, String)> run(Stream<String> iStream) async* {
-    final iStreamIterator = StreamIterator<String>(iStream);
+    // クライアントからの入力ストリーム（イテレータ）
+    final is_ = StreamIterator<String>(iStream);
 
     const aiGcps = [Gcp.g, Gcp.c, Gcp.p];
     const aiDirs = [Dir.up, Dir.down, Dir.left, Dir.right];
 
     int revision = 0;
     for (;;) {
-      for (bool isJkp = true;; isJkp = false) {
+      for (_isJkp = true;;) {
         // 一回目ならジャンケンポン、二回目以後ならあいこでしょ
-        final mode = isJkp ? Mode.jkp : Mode.aks;
-        yield (revision++, '{"mode":"${mode.name}", "isJkp":$isJkp}');
+        _mode = _isJkp! ? Mode.jkp : Mode.aks;
+        // その他の初期値
+        _aiGcp = _huGcp = _aiDir = _aiDir = null;
+        // ジャンケン入力画面（初回get）
+        yield (_revision++, '');
+
         // AI選択
         final aiGcp = aiGcps[Random().nextInt(aiGcps.length)];
-        // 人間入力待ち
-        iStreamIterator.moveNext();
-        final huGcp = Gcp.values.byName(iStreamIterator.current);
-        if (aiGcp != huGcp) {
-          // あいこでなければ
-          final huWin = huGcp.wins(aiGcp);
-          // あっちむいてホイ
-          yield (revision++, '{"mode":"${Mode.amh.name}", "huWin":$huWin}');
-          // AI方向選択
-          final aiDir = aiDirs[Random().nextInt(aiDirs.length)];
-          // 人間方向入力待ち
-          iStreamIterator.moveNext();
-          final huDir = Dir.values.byName(iStreamIterator.current);
-          if (aiDir == huDir) {
-            // 勝敗決定
-            yield (revision++, '{"mode":"${huWin ? Mode.huWin.name : Mode.aiWin.name}"}');
-          } else {
-            // 引き分け
-            yield (revision++, '{"mode":"${Mode.draw.name}"}');
-          }
-          // 次へ
-          iStreamIterator.moveNext();
+        // 人間のジャンケンpost待ち
+        await is_.moveNext();
+        // todo: 入力バリデーション
+        final huGcp = Gcp.values.byName(is_.current);
+
+        if (aiGcp == huGcp) {
+          // あいこの場合、ジャンケンに戻る
+          _isJkp = false;
+          continue;
         }
+
+        // あっちむいて入力画面
+        _mode = aiGcp.wins(huGcp) ? Mode.aiAmh : Mode.huAmh;
+        yield (revision++, '');
+
+        // AI方向選択
+        _aiDir = aiDirs[Random().nextInt(aiDirs.length)];
+        // 人間方向post待ち
+        await is_.moveNext();
+        // todo: 入力バリデーション
+        _huDir = Dir.values.byName(is_.current);
+
+        if (_aiDir == _huDir) {
+          // 勝敗画面
+          _mode = (_mode == Mode.aiAmh) ? Mode.aiWin : Mode.huWin;
+        } else {
+          // 引き分け画面
+          _mode = Mode.draw;
+        }
+        // OK入力待ち
+        yield (revision++, '');
+        is_.moveNext();
       }
     }
   }
-}
 
-Stream<(int, String)> _jkpAmhLogic(Stream<String> receiveStream) async* {
-  logger.fine('[i]_jkpAmhLogic');
-
-  final receiveStreamIterator = StreamIterator(receiveStream);
-
-  int revision = 0;
-  String state = '{}';
-
-  for (;;) {
-    Mode mode = Mode.jkp;
-    yield (revision, '{"mode":"${mode.name}"}');
-    final huGcp = await receiveStreamIterator.moveNext();
-
-    switch (receiveStreamIterator.current) {
-      case 'get':
-        logger.fine('_counterLogic.get');
-        yield (revision, state);
-      case 'post':
-        logger.fine('_counterLogic.post');
-        ++revision;
-        state = revision.toString();
-        yield (revision, state);
-      default:
-        throw UnimplementedError();
-    }
+  @override
+  String filterState(String args, String state) {
+    final map = {
+      'mode': _mode?.name,
+      'isJkp': _isJkp,
+      'aiGcp': _aiGcp?.name,
+      'huGcp': _huGcp?.name,
+      'aiDir': _aiDir?.name,
+      'huDir': _huDir?.name,
+    };
+    return jsonEncode(map);
   }
 }
