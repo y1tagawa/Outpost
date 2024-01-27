6e3461140bde93e95fb1cff43a5237a385a0d0f3
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2024-01-27 17:53:39 +0900

    add flutter ditredi

---
+++ FlutterProjects/hello_flutter_ditredi/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:ditredi/ditredi.dart';
+import 'package:flutter/material.dart' as material show Colors;
+import 'package:flutter/material.dart' hide Colors;
+import 'package:vector_math/vector_math_64.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: material.Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: DiTreDi(
+        figures: [
+          Cube3D(2, Vector3(0, 0, 0)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
