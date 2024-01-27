98ad8fd031f537b1e720962b2e72d6805e211667
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2024-01-27 18:47:04 +0900

    Update main.dart

--- FlutterProjects/hello_flutter_ditredi/lib/main.dart
+++ FlutterProjects/hello_flutter_ditredi/lib/main.dart
@@ -1,10 +1,27 @@
 import 'package:ditredi/ditredi.dart';
 import 'package:flutter/material.dart' as material show Colors;
 import 'package:flutter/material.dart' hide Colors;
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logging/logging.dart';
 import 'package:vector_math/vector_math_64.dart';
 
+final _d3dControllerProvider = ChangeNotifierProvider(
+  (ref) => DiTreDiController(
+    rotationX: 0.0,
+    rotationY: 0.0,
+    //translation: const Offset(0, 0),
+  ),
+);
+
+final _logger = Logger('hello_flutter_two_dimensional');
+
 void main() {
-  runApp(const MyApp());
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    debugPrint('[${record.loggerName}] ${record.level.name}: ${record.time}: ${record.message}');
+  });
+
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -23,41 +40,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends HookConsumerWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = ref.watch(_d3dControllerProvider);
 
-class _MyHomePageState extends State<MyHomePage> {
-  late final DiTreDiController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = DiTreDiController(rotationX: 0.0, rotationY: 0.0);
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: DiTreDi(
         figures: [
-          Cube3D(2, Vector3(0, 0, 0)),
+          for (double x = -12.0; x < 12.0; x += 3.0)
+            for (double z = 0.0; z < 20.0; z += 3.0) Cube3D(2, Vector3(x, 0, z)),
         ],
-        controller: _controller,
+        controller: controller,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _controller.update(
-            rotationY: _controller.rotationY + 10.0,
+          controller.update(
+            rotationY: controller.rotationY + 10.0,
           );
         },
         tooltip: 'Increment',
